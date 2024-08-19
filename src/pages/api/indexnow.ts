183d66e0b9aba89d36c301d9d9685288d943a2d0
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-08-19 15:11:50 +0600

    Update indexnow.ts

--- src/pages/api/indexnow.ts
+++ src/pages/api/indexnow.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
-
+ 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { url } = req.query;
 
