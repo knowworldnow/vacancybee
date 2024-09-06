8601b1ac205c0c28e8747d1fc07f8ff7e73d51eb
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-06 20:27:06 +0600

    Update UbCallToActionBlock.js

--- src/wp-blocks/UbCallToActionBlock.js
+++ src/wp-blocks/UbCallToActionBlock.js
@@ -1,38 +1,8 @@
-import React from 'react';
-import { gql } from '@apollo/client';
-
 export default function UbCallToActionBlock(props) {
   console.log(props.attributes);
-  return <div>UbCallToActionBlock</div>;
+  return <div>UbCallToActionBlock</div>
 }
 
 UbCallToActionBlock.displayName = 'UbCallToActionBlock';
-
-UbCallToActionBlock.fragments = {
-  key: `UbCallToActionBlockFragment`,
-  entry: gql`
-    fragment UbCallToActionBlockFragment on UbCallToActionBlock {
-      attributes {
-        ctaBackgroundColor
-        ctaBorderSize
-        ctaBorderColor
-        headFontSize
-        headColor
-        headAlign
-        ubCallToActionHeadlineText
-        contentFontSize
-        contentColor
-        contentAlign
-        ubCtaContentText
-        buttonColor
-        buttonWidth
-        url
-        buttonTextColor
-        buttonFontSize
-        ubCtaButtonText
-        addNofollow
-        openInNewTab
-      }
-    }
-  `,
-};
+// This also works
+// UbCallToActionBlock.config.name = 'UbCallToActionBlock'
