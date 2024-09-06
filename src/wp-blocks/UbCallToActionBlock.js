6b88d3a39d7e7d15cd062f9a055a22afbd368261
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-06 20:43:14 +0600

    Delete src/wp-blocks/UbCallToActionBlock.js

--- src/wp-blocks/UbCallToActionBlock.js
+++
@@ -1,8 +0,0 @@
-export default function UbCallToActionBlock(props) {
-  console.log(props.attributes);
-  return <div>UbCallToActionBlock</div>
-}
-
-UbCallToActionBlock.displayName = 'UbCallToActionBlock';
-// This also works
-// UbCallToActionBlock.config.name = 'UbCallToActionBlock'
